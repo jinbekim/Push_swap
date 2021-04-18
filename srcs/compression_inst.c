cd53bae2610228b9a9a1900df5f361edac66312c
jinbekim <jinbekim@student.42seoul.kr>
2021-04-18 14:24:21 +0900

    wootaa

--- srcs/compression_inst.c
+++ srcs/compression_inst.c
@@ -22,6 +22,50 @@ static void	find_pare(t_list *tmp, char *pare)
 	}
 }
 
+static void	find_pare2(t_list *tmp, char *pare)
+{
+	t_list	*tmp2;
+	t_list	*tmp3;
+
+	tmp2 = tmp;
+	while (tmp2->next && (ft_strncmp((char *)tmp2->content, "sr", 3) == 0 \
+	 || ft_strncmp((char *)tmp2->content, "sa", 3) == 0 \
+	 || ft_strncmp((char *)tmp2->content, "sb", 3) == 0))
+	{
+		if (ft_strncmp((char *)tmp2->next->content, pare, 3) == 0)
+		{
+			((char *)tmp->content)[1] = 'r';
+			tmp3 = tmp2->next;
+			tmp2->next = tmp3->next;
+			free(tmp3->content);
+			free(tmp3);
+		}
+		tmp2 = tmp2->next;
+	}
+}
+
+static void	find_pare3(t_list *tmp, char *pare)
+{
+	t_list	*tmp2;
+	t_list	*tmp3;
+
+	tmp2 = tmp;
+	while (tmp2->next && (ft_strncmp((char *)tmp2->content, "rrr", 4) == 0 \
+	 || ft_strncmp((char *)tmp2->content, "rra", 4) == 0 \
+	 || ft_strncmp((char *)tmp2->content, "rrb", 4) == 0))
+	{
+		if (ft_strncmp((char *)tmp2->next->content, pare, 4) == 0)
+		{
+			((char *)tmp->content)[2] = 'r';
+			tmp3 = tmp2->next;
+			tmp2->next = tmp3->next;
+			free(tmp3->content);
+			free(tmp3);
+		}
+		tmp2 = tmp2->next;
+	}
+}
+
 void	compression_inst(t_list **inst)
 {
 	t_list	*tmp;
@@ -33,6 +77,14 @@ void	compression_inst(t_list **inst)
 			find_pare(tmp, "rb");
 		else if (ft_strncmp((char *)tmp->content, "rb", 3) == 0)
 			find_pare(tmp, "ra");
+		else if (ft_strncmp((char *)tmp->content, "sa", 3) == 0)
+			find_pare2(tmp, "sb");
+		else if (ft_strncmp((char *)tmp->content, "sb", 3) == 0)
+			find_pare2(tmp, "sa");
+		else if (ft_strncmp((char *)tmp->content, "rra", 4) == 0)
+			find_pare3(tmp, "rrb");
+		else if (ft_strncmp((char *)tmp->content, "rrb", 4) == 0)
+			find_pare3(tmp, "rra");
 		tmp = tmp->next;
 	}
 }
